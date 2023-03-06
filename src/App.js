03e741434a554d5ca71995b5ab1689bb6255ed20
lags2022 <lguzman.58erb@outlook.com>
2023-03-06 13:04:09 -0500

    update

--- src/App.js
+++ src/App.js
@@ -5,12 +5,14 @@ import Notes from "./components/Notes";
 import Notification from "./components/Notification";
 import noteService from "./services/notes";
 import { useState, useEffect } from "react";
+import loginService from "./services/login";
 
 function App() {
   const [notes, setNotes] = useState([]);
   const [newNote, setNewNote] = useState("");
   const [showAll, setShowAll] = useState(true);
   const [errorMessage, setErrorMessage] = useState(null);
+  const [user, setUser] = useState(null);
 
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
@@ -21,13 +23,26 @@ function App() {
     });
   }, []);
 
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem("loggedNoteappUser");
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON);
+      setUser(user);
+      noteService.setToken(user.token);
+    }
+  }, []);
+
+  const handleLogout = () => {
+    setUser(null);
+    noteService.setToken(user.token);
+    window.localStorage.removeItem("loggedNoteappUser");
+  };
+
   const addNote = (event) => {
     event.preventDefault();
     const noteObject = {
       content: newNote,
-      date: new Date().toISOString(),
       important: Math.random() < 0.5, //esto pa q ya salga en true o false de forma aleatoria
-      id: notes.length + 1,
     };
 
     noteService.create(noteObject).then((returnedNote) => {
@@ -60,12 +75,76 @@ function App() {
     setNewNote(event.target.value);
   };
 
+  const handleLogin = async (event) => {
+    event.preventDefault();
+
+    try {
+      const user = await loginService.login({ username, password });
+
+      window.localStorage.setItem("loggedNoteappUser", JSON.stringify(user));
+
+      noteService.setToken(user.token);
+
+      setUser(user);
+      setUsername("");
+      setPassword("");
+    } catch (error) {
+      setErrorMessage("Wrong credentials");
+      setTimeout(() => {
+        setErrorMessage(null);
+      }, 5000);
+    }
+  };
+
   const notesToShow = showAll ? notes : notes.filter((note) => note.important);
+
+  const renderLoginForm = () => {
+    return (
+      <form onSubmit={handleLogin}>
+        <div>
+          <input
+            type="text"
+            value={username}
+            name="Username"
+            placeholder="Username"
+            onChange={({ target }) => setUsername(target.value)}
+          />
+        </div>
+        <div>
+          <input
+            type="password"
+            value={password}
+            name="Password"
+            placeholder="Password"
+            onChange={({ target }) => setPassword(target.value)}
+          />
+        </div>
+        <button type="submit">Login</button>
+      </form>
+    );
+  };
+
+  const renderCreateNoteForm = () => {
+    return (
+      <>
+        <form onSubmit={addNote}>
+          <input
+            placeholder="Write a note content"
+            value={newNote}
+            onChange={handleNoteChange}
+          />
+          <button type="submit">save</button>
+        </form>
+        <button onClick={handleLogout}>Cerrar sesión</button>
+      </>
+    );
+  };
 
   return (
     <div className="App">
       <h1>Notes</h1>
       <Notification message={errorMessage} />
+      {user ? renderCreateNoteForm() : renderLoginForm()}
       <div>
         <button onClick={() => setShowAll(!showAll)}>
           show {showAll ? "important" : "all"}
@@ -80,10 +159,6 @@ function App() {
           />
         ))}
       </ul>
-      <form onSubmit={addNote}>
-        <input value={newNote} onChange={handleNoteChange} />
-        <button type="submit">save</button>
-      </form>
     </div>
   );
 }
--- src/services/notes.js
+++ src/services/notes.js
@@ -1,19 +1,33 @@
 import axios from "axios";
 const baseUrl = "http://localhost:3001/api/notes";
 
+let token = null;
+
+const setToken = (newToken) => {
+  token = `Bearer ${newToken}`;
+};
+
 const getAll = () => {
   const request = axios.get(baseUrl);
   return request.then((response) => response.data);
 };
 
 const create = (newObject) => {
-  const request = axios.post(baseUrl, newObject);
+  const config = {
+    headers: { Authorization: token },
+  };
+
+  const request = axios.post(baseUrl, newObject, config);
   return request.then((response) => response.data);
 };
 
 const update = (id, newObject) => {
-  const request = axios.put(`${baseUrl}/${id}`, newObject);
+  const config = {
+    headers: { Authorization: token },
+  };
+
+  const request = axios.put(`${baseUrl}/${id}`, newObject, config);
   return request.then((response) => response.data);
 };
 
-export default { getAll, create, update };
+export default { getAll, create, update, setToken };
